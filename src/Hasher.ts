fabae12ee8694fe7fe908d38fd68996e92176aa3
komed3 <pk97@outlook.de>
2026-03-22 10:08:58 +0100

    implement murmur3

--- src/Hasher.ts
+++ src/Hasher.ts
@@ -1,6 +1,6 @@
 export class Hasher {
 
-    public static FNV1a ( str: string ) : number {
+    public static fnv1a ( str: string ) : number {
         let hash = 0x811c9dc5;
 
         for ( let i = 0; i < str.length; i++ ) {
@@ -11,11 +11,48 @@ export class Hasher {
         return hash >>> 0;
     }
 
-    public static fastFNV1a ( str: string ) : number {
-        const len = str.length;
-        const limit = len & ~3;
-        let hash = 0x811c9dc5;
-        let i = 0;
+    public static murmur3 ( str: string ) : number {
+        const len = str.length, limit = len & ~3;
+        let hash = 0, i = 0;
+
+        for ( ; i < limit; i += 4 ) {
+            let chunk = str.charCodeAt( i ) |
+                ( str.charCodeAt( i + 1 ) << 8 ) |
+                ( str.charCodeAt( i + 2 ) << 16 ) |
+                ( str.charCodeAt( i + 3 ) << 24 );
+
+            chunk = Math.imul( chunk, 0xcc9e2d51 );
+            chunk = ( chunk << 15 ) | ( chunk >>> 17 );
+            chunk = Math.imul( chunk, 0x1b873593 );
+
+            hash ^= chunk;
+            hash = ( hash << 13 ) | ( hash >>> 19 );
+            hash = Math.imul( hash, 5 ) + 0xe6546b64;
+        }
+
+        for ( ; i < len; i++ ) {
+            let k = str.charCodeAt( i );
+
+            k = Math.imul( k, 0xcc9e2d51 );
+            k = ( k << 15 ) | ( k >>> 17 );
+            k = Math.imul( k, 0x1b873593 );
+
+            hash ^= k;
+        }
+
+        hash ^= len;
+        hash ^= hash >>> 16;
+        hash *= 0x85ebca6b;
+        hash ^= hash >>> 13;
+        hash *= 0xc2b2ae35;
+        hash ^= hash >>> 16;
+
+        return hash >>> 0;
+    }
+
+    public static fasthash ( str: string ) : number {
+        const len = str.length, limit = len & ~3;
+        let hash = 0x811c9dc5, i = 0;
 
         for ( ; i < limit; i += 4 ) {
             const chunk = str.charCodeAt( i ) |
