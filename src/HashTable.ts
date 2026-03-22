c1cad2870576cbeb0b10895884810949c0918241
komed3 <pk97@outlook.de>
2026-03-22 15:53:15 +0100

    set up hash table

--- src/HashTable.ts
+++ src/HashTable.ts
@@ -1,1 +1,30 @@
-export class HashTable {}
+import { Hasher } from './Hasher';
+
+export type HashFn = ( str: string, seed?: number ) => number;
+export type Hash = 'fasthash' | 'fnv1a' | 'murmur3' | HashFn;
+
+export interface HashOptions {
+    hash?: Hash;
+}
+
+
+const DEFAULT_OPTIONS : HashOptions = {
+    hash: 'fasthash'
+};
+
+
+export class HashTable {
+
+    private readonly options: HashOptions;
+    private readonly hash: HashFn;
+    private table = new Map< string, any > ();
+
+    constructor ( options: HashOptions = {} ) {
+        this.options = { ...DEFAULT_OPTIONS, ...options };
+
+        try { this.hash = typeof this.options.hash === 'function' ? this.options.hash
+            : this.options.hash = Hasher[ this.options.hash as keyof Hasher ] }
+        catch ( err ) { throw Error ( `Cannot set hasher function`, { cause: err } ) }
+    }
+
+}
