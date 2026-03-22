ce2eefaa7c2e619f21c4c82ca5ddab2bf3ed2eb7
komed3 <pk97@outlook.de>
2026-03-22 09:57:48 +0100

    implement classic FNV-1a

--- src/Hasher.ts
+++ src/Hasher.ts
@@ -1,6 +1,15 @@
 export class Hasher {
 
-    public static FNV1a ( str: string ) : number {}
+    public static FNV1a ( str: string ) : number {
+        let hash = 0x811c9dc5;
+
+        for ( let i = 0; i < str.length; i++ ) {
+            hash ^= str.charCodeAt( i );
+            hash = Math.imul( hash, 0x01000193 );
+        }
+
+        return hash >>> 0;
+    }
 
     public static fastFNV1a ( str: string ) : number {
         const len = str.length;
