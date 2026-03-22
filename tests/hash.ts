165f205a1d4a5eb42d676c62d654f8ea73138723
komed3 <pk97@outlook.de>
2026-03-22 12:23:24 +0100

    Update hash.ts

--- tests/hash.ts
+++ tests/hash.ts
@@ -1,15 +1,13 @@
 import { Hasher } from '../src/Hasher.ts';
 
-const str = 'Hello World ;)';
-
 let start = process.hrtime.bigint();
-for ( let i = 0; i < 1e6; i++ ) Hasher.fasthash( str );
-console.log( 'Fast hash:', Number( process.hrtime.bigint() - start ) / 1e6, 'ns' );
-
-start = process.hrtime.bigint();
-for ( let i = 0; i < 1e6; i++ ) Hasher.fnv1a( str );
+for ( let i = 0; i < 1e6; i++ ) Hasher.fnv1a( 'test_' + i );
 console.log( 'FNV-1a', Number( process.hrtime.bigint() - start ) / 1e6, 'ns' );
 
 start = process.hrtime.bigint();
-for ( let i = 0; i < 1e6; i++ ) Hasher.murmur3( str );
+for ( let i = 0; i < 1e6; i++ ) Hasher.fasthash( 'test_' + i );
+console.log( 'Fast hash:', Number( process.hrtime.bigint() - start ) / 1e6, 'ns' );
+
+start = process.hrtime.bigint();
+for ( let i = 0; i < 1e6; i++ ) Hasher.murmur3( 'test_' + i );
 console.log( 'Murmur3', Number( process.hrtime.bigint() - start ) / 1e6, 'ns' );
