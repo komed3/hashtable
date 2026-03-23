e5a3a309d6f4d49a5bf46c4642cc47a4baf1f1fd
komed3 <pk97@outlook.de>
2026-03-23 14:22:01 +0100

    add rollup config

---
+++ rollup.config.mjs
@@ -0,0 +1,26 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import cleanup from 'rollup-plugin-cleanup';
+import prettier from 'rollup-plugin-prettier';
+
+export default {
+    input: 'src/HashTable.ts',
+    plugins: [
+        resolve( { extensions: [ '.js', '.ts' ] } ), commonjs(),
+        typescript( { tsconfig: 'tsconfig.json', compilerOptions: { declaration: false } } ),
+        cleanup( { comments: 'istanbul', extensions: [ 'js', 'ts' ] } ),
+        prettier( {
+            parser: 'babel', tabWidth: 2, bracketSpacing: true, bracketSameLine: true, singleQuote: true,
+            jsxSingleQuote: true, trailingComma: 'none', objectWrap: 'collapse'
+        } )
+    ],
+    output: [ {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        exports: 'named'
+    }, {
+        file: 'dist/index.mjs',
+        format: 'es'
+    } ]
+};
