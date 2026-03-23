d2d7512d392d9c8ac8d1adb26e17dd77acbc0eac
komed3 <pk97@outlook.de>
2026-03-23 14:07:26 +0100

    add some tests

--- tests/hash.ts
+++ tests/hash.ts
@@ -1,13 +1,12 @@
-import { Hasher } from '../src/Hasher.ts';
+import { HashTable } from '../src/HashTable.ts';
 
-let start = process.hrtime.bigint();
-for ( let i = 0; i < 1e6; i++ ) Hasher.fnv1a( 'test_' + i );
-console.log( 'FNV-1a', Number( process.hrtime.bigint() - start ) / 1e6, 'ns' );
+const table = new HashTable( { hash: 'fnv1a' } );
+const arr = [ 'Hello World', 'Some test', 'Another test', 'Hello World' ];
 
-start = process.hrtime.bigint();
-for ( let i = 0; i < 1e6; i++ ) Hasher.fasthash( 'test_' + i );
-console.log( 'Fast hash:', Number( process.hrtime.bigint() - start ) / 1e6, 'ns' );
+for ( const str of arr ) {
+    const key = table.key( [ str ] );
+    if ( key && table.has( key ) ) console.log( 'Duplicate:', str );
+    else if ( key ) table.set( key, str );
+}
 
-start = process.hrtime.bigint();
-for ( let i = 0; i < 1e6; i++ ) Hasher.murmur3( 'test_' + i );
-console.log( 'Murmur3', Number( process.hrtime.bigint() - start ) / 1e6, 'ns' );
+console.log( table.size() );
